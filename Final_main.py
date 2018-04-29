5cc5e627ea0836d87f069b598bb775f50c666229
William Breen <wbreen@duke101-09.elon.edu>
2018-04-29 18:16:10 -0400

    added the board detection to the final main file

--- Final_main.py
+++ Final_main.py
@@ -13,10 +13,17 @@ import game_logic as g_log
 import board_recognizer as br
 
 #read in image that the board is in
+boards = 'grid_photos/'
+grid_size = 500
+grids = hm.read_in_files(boards)
+final_boards = []
 
-#identify the board
-
-#identify the squares in the board
+#identify the board (need an output size)
+for g in grids:
+    board = br.find_playing_board(g, grid_size)
+    final_boards.append(board)
+board = final_boards[1]
+#identify the squares in the board (Hough lines?)
 
 #identify if there are any marks on the board (hit or miss)
 
